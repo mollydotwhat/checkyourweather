630aedda8685428712b959b146ff3afc612ae96a
molly <molly.salzberger@gmail.com>
2023-12-14 04:26:45 -0600

    clearer notes

--- assets/script.js
+++ assets/script.js
@@ -25,13 +25,14 @@ function getWeather(city) {
     // replace London,uk programatically
     
     //SAVE city searched to search history list
-    
+
     var citySearched = `api.openweathermap.org/data/2.5/weather?q=${city}&APPID=fb19d043c4c8d10594e04cf27975c0d5`
     //fetch (async)
     fetch(citySearched).then(function (response) {
         console.log(response.json)
         return response.json;
     }).then(function (data){
+        // first day in list = today
         weatherToday(data.list[0]);
         showFive(data.coord.lat, data.coord.lon);
     })
@@ -42,7 +43,7 @@ function getWeather(city) {
 //params from getweather
 function weatherToday(currentCity) {
     //build today section w/info from search
-    var cityName;
+    var cityName = currentCity.name;
     var currentDate;
     var currentTemp;
     var currentWind;
@@ -59,7 +60,7 @@ function showFive(lat, lon) {
     }).then(function (data){
         var cityName = data.name;
         //header w/city name
-        // loop through data list by index (day), generating card w/each date, temp, wind, humidity
+        // loop through data list by index (less than 5 because array), generating card w/each date, temp, wind, humidity
 
 
     })
