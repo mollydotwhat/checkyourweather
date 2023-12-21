df3733e191075d6a9d917f4a6d1069fef4e3a90c
molly <molly.salzberger@gmail.com>
2023-12-21 11:05:45 -0600

    getting coords correctly from name, and city weather from coords. console.log is king)

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,48 +15,53 @@ var keyCode = "fb19d043c4c8d10594e04cf27975c0d5";
 // search function (get user input from form field)
 function getLatLon (nameCity) {
     event.preventDefault();
-  
+    
     var findCoords = `http://api.openweathermap.org/geo/1.0/direct?q=${nameCity}&limit=5&appid=${keyCode}`
-
+    // console.log(findCoords);
     fetch(findCoords)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+        console.log(data)
+        // let lat = JSON.stringify(data[0].lat);
+        // let lon = JSON.stringify(data[0].lon)
+        getWeather(data[0].lat, data[0].lon);
         // select & return location coords from object returned
-        return cityCoords = [data[0].lat, data[0].lon]
+        // return cityCoords = {
+        //     latitude: data.lat, 
+        //     longitude: data.lon
+        // }
     })
-    // JUST in case because scope is difficult sometimes
-    return cityCoords;
+
 };
 
  function searchOne () {
-   getWeather();
+    var cityName = cityInput.value;
+    getLatLon(cityName);
+//    getWeather();
  };
 
-searchBtnOne.on("click", searchOne);
+searchBtnOne.addEventListener("click", searchOne);
 
 // get weather api
-function getWeather() {
-    event.preventDefault();
-    let cityName = cityInput.val().trim();
-    console.log(cityName);
-    var coordSet = getLatLon(cityName);
-    var cityLat = coordSet[0];
-    var cityLon = coordSet[1];
-    console.log(cityLat);
-    console.log(cityLon);
+function getWeather(lat, lon) {
+
     // using coords returned from getLatLon to search weather app. I might need more async syntax for this, but trying it this way first.
-    var citySearched = `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${keyCode}`
+    var citySearched = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${keyCode}`
 
     fetch(citySearched)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-        console.log(data)
-        // buildToday(data);
-        // build5Day(data);
+        // console.log(data)
+        console.log(`city name: ` + JSON.stringify(data.name))
+        console.log(`weather search result: ` + JSON.stringify(data))
+        // buildToday(data)
+        // build5Day(data)
+
+
     })
 
 };
